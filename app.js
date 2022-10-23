dbaeda2835838af02af07241a99b207efaed3f73
Javohir <qamariddinovjavohir825@gmail.com>
2022-10-23 19:05:29 +0500

    faylda o;zgarish boldi

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const app = express()
+const port = 3030 || process.env.PORT
+
+app.get('/', (req, res) =>{
+    res.send('Hello World!')
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`server running`)
+})
